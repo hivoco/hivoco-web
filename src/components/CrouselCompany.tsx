beae94968835340db94e563b2ba6263e9d526a2d
Krishna-hivoco <krishna@hivoco.com>
2024-10-28 18:15:24 +0530

    Bug and social media icon

--- src/components/CrouselCompany.tsx
+++ src/components/CrouselCompany.tsx
@@ -10,13 +10,13 @@ function CrouselCompany() {
     <div className="overflow-hidden w-screen">
       <div className="flex animate-scroll py-10 px-3  gap-14 whitespace-nowrap">
         {infiniteLogos.map((l, index) => (
-          <div key={index} className="inline-block flex-shrink-0">
+          <div key={index} className="inline-block flex-shrink-0 ">
             <Image
               src={`/brand/${l}.png`}
               alt="example"
               width={150}
               height={150}
-              className="object-cover w-auto h-12 rounded-lg"
+              className="object-cover w-auto h-12 rounded-lg grayscale"
             />
           </div>
         ))}
