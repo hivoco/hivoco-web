0d7421f8eba147d656f40a13c525e12be4f02cc0
Krishna-hivoco <krishna@hivoco.com>
2024-10-28 13:06:00 +0530

    Adding font paragraph and heading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,11 +15,11 @@ const Hero = ({ handleScroll }: props) => {
         backgroundBlendMode: "overlay",
       }}
     >
-      <div className="flex container flex-col justify-center items-center py-28 gap-8 text-center">
-        <h2 className="font-sf-pro-display-bold text-3xl md:text-4xl lg:text-5xl font-bold">
+      <div className="flex container flex-col justify-center items-center py-28 gap-7 text-center">
+        <h2 className="font-sf-pro-display-bold text-3xl xl:text-5xl font-bold">
           Lorem ipsum dolor sit <br /> amet consectetur.
         </h2>
-        <p className="text-p-gray font-sf-pro-display-normal text-sm lg:text-base">
+        <p className="text-p-gray font-sf-pro-display-normal text-sm lg:text-lg">
           Lorem ipsum dolor sit amet consectetur. Sed <br /> pulvinar id leo eu
           imperdiet platea dui. Lorem <br /> ipsum dolor sit amet consectetur.
         </p>
